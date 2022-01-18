bfc347abc763cbd0cd736e51ef0f034afaddfcdc
Sai Varma Penmatsa <67587377+SAIVARMA571@users.noreply.github.com>
2022-01-18 14:45:01 -0500

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,32 @@
+randomNumber1 = Math.floor((Math.random()*6) +1 );
+randomNumber2 = Math.floor((Math.random()*6) +1 );
+
+
+function addImage(randomNumber1) {
+                document.querySelector(".img1").setAttribute("src",`images/dice${randomNumber1}.png` );
+
+ }
+
+ function addImage2(randomNumber2) {
+                document.querySelector(".img2").setAttribute("src",`images/dice${randomNumber2}.png` );
+
+ }
+
+
+function whoWon(randomNumber1, randomNumber2) {
+                if (randomNumber1 > randomNumber2) {
+                 document.querySelector("h1").innerHTML = "🚩Player1 Wins ";
+         }
+         else if(randomNumber1 < randomNumber2) {
+                 document.querySelector("h1").innerHTML = "Player2 Wins🚩 ";}
+
+         else {
+                document.querySelector("h1").innerHTML = "Draw!";
+         }
+}
+
+
+addImage(randomNumber1);
+addImage2(randomNumber2);
+
+whoWon(randomNumber1, randomNumber2);
